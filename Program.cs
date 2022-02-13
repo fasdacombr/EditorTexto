48be1790ba8550009fb925c69ef4308e602abb3c
Flavio <flavio_125@hotmail.com>
2022-02-13 20:29:21 -0300

    Finalizando o Editor de texto

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace TextEditor
 {
@@ -25,17 +26,57 @@ namespace TextEditor
                 case 2: Editar(); break;
                 default: Menu(); break; 
             }
-
-            static void Abrir()
-            {
-
-            }
-
-            static void Editar()
-            {
-
-            }
         }
 
+        static void Abrir()
+        {
+            Console.Clear();
+            Console.WriteLine("Qual o caminho do arquivo");
+            string path = Console.ReadLine();
+
+            using(var file = new StreamReader(path))
+            {
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+
+            Console.WriteLine("");
+            Console.ReadLine();
+            Menu();
+        }
+
+        static void Editar()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite seu texto abaixo: (ESC para sair)");
+            Console.WriteLine("------------------------");
+            string text = "";
+
+            do
+            {
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Salvar(text);
+
+        }
+
+        static void Salvar(string text)
+        {
+            Console.Clear();
+            Console.WriteLine("Qual caminho para salvar o arquivo?");
+            var path = Console.ReadLine();
+
+            using(var file = new StreamWriter(path))
+            {
+                file.Write(text);
+            }
+
+            Console.WriteLine($"Arquivo salvo em {path} com sucesso!!!");
+            Console.ReadLine();
+            Menu();
+        }
     }
 }
